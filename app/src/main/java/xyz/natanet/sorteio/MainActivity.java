11f26524b81eb76c8ad8bef36d8bd6bd98cfe44e
natanasilva <nataniel.natanaguiar@gmail.com>
2022-11-15 14:33:21 -0300

    Finalizando o Projeto

--- app/src/main/java/xyz/natanet/sorteio/MainActivity.java
+++ app/src/main/java/xyz/natanet/sorteio/MainActivity.java
@@ -3,12 +3,24 @@ package xyz.natanet.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random gerador = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+        TextView texto = findViewById(R.id.numeroSelecionado);
+            texto.setText("O numero sorteado foi: " + gerador.nextInt(10));
     }
 }
